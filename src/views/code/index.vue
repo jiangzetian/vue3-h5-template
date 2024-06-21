cdf0d4cd28626dd7134bd8aa7be50b26669be5ff
蒋泽天 <1139246375@qq.com>
2024-06-22 01:54:04 +0800

    feat：清除多余代码

--- src/views/code/index.vue
+++ src/views/code/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view />
+  <h1>code</h1>
 </template>
 
 <script setup lang="ts"></script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view />
+  <h1>home</h1>
 </template>
 
 <script setup lang="ts"></script>
